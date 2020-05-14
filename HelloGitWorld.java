ed1fbe4cde65cc2b354236cbe8dae845763a90b0
Lynn <schnoorld@gmail.com>
2020-05-14 14:29:16 -0500

    Add HelloWorld

---
+++ HelloGitWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloGitWorld {
+
+	public static void main(String[] args) {
+		// Let's practice using Git and GitHub
+		System.out.println("This is the first line of my code.");
+		System.out.println("Please add some content here to make this better!");
+		
+
+	}
+
+}
